9c83465f07269791476b426401d05cdcd1b95792
PC-20171108YUJV\Administrator <2297999137@qq.com>
2019-06-06 16:43:55 +0800

    no message

--- src/Classtest.php
+++ src/Classtest.php
@@ -1,7 +1,10 @@
 <?php
 
-namespace sugao2013\composer_test\src;
-
+namespace sugao2013\composer_test;
+//namespace sugao2013\composer_test\src;
+// "psr-4": {
+//            "sugao2013\\composer_test\\": ""
+//        }
 class ClassTest {
 
     public function class_test(){
